5a4a075c673a6c6ffa6fb945fc1b227cf668654b
Tugberk Ugurlu <tugberk@outlook.com>
2023-11-24 23:06:35 +0000

    initial impl
    server
    
    ```
    @tugberkugurlu ➜ /workspaces/go-grpc-example (main) $ go run server.go
    2023/11/24 23:05:17 server listening at [::]:50051
    2023/11/24 23:05:31 received:
    2023/11/24 23:05:46 received:
    ```
    
    client:
    
    ```
    @tugberkugurlu ➜ /workspaces/go-grpc-example (main) $ grpcurl -plaintext localhost:50051 helloworld.Greeter/SayHello
    {
      "message": "Hello "
    }
    ```

--- server.go
+++ server.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/tugberkugurlu/go-grpc-example/spec"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	spec.UnimplementedGreeterServer
+}
+
+func (*server) SayHello(ctx context.Context, in *spec.HelloRequest) (*spec.HelloReply, error) {
+	log.Printf("received: %v", in.GetName())
+	return &spec.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
 
 func main() {
-	fmt.Println("hello grpc")
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+	spec.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
